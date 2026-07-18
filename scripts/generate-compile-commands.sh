51bf30f0c6679237131d5be0a73e60219bab549d
Nicholas Ammann <ammannnicholas@gmail.com>
2026-07-18 08:45:15 -0700

    Add script for generating the compile_command.json file needed for clangd intellisense

---
+++ scripts/generate-compile-commands.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# pipx is needed to install compiledb (python package) on your system
+if command -v pipx &> /dev/null; then
+    echo "pipx is installed."
+else
+    echo "Error: pipx is NOT installed, please install"
+    return 1
+fi
+
+if pipx list | grep -q "compiledb"; then
+    echo "compiledb is installed via pipx."
+else
+    echo "Error: compiledb is missing, please install with 'pipx install compiledb'"
+    return 1
+fi
+
+# need to install compiledb python package
+pushd ../
+compiledb make
+popd
